b9c9f2bbe2ff1c736f3856a9fe8887f0b0f8074c
NewUserName <ramanpal7228@gmail.com>
2025-06-20 14:27:42 +0530

    type of Done

--- Javascript/file1.js
+++ Javascript/file1.js
@@ -1,4 +1,19 @@
-let a=10
-let b=20
-let c=a+b
-console.log(c)
+// let a=10
+// let b=20
+// let c=a+b
+// console.log(c)
+// let studentName ="John";
+// let age=20;
+// let prompted=true
+// let score
+// let city=null
+// console.log(studentName)
+// console.log(typeof studentName)
+// console.log(typeof a)
+// console.log(typeof prompted)
+// console.log(typeof score)
+// console.log(typeof city)
+let age="10";
+console.log(typeof age)
+let ageNum = Number(age);
+console.log(typeof ageNum);
