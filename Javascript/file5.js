775e06d65247681777ec4f6b4171ac2e0ed88534
NewUserName <ramanpal7228@gmail.com>
2025-06-20 15:22:37 +0530

    functions added

--- Javascript/file5.js
+++ Javascript/file5.js
@@ -1,5 +1,31 @@
 //regular function
-function greet(){
-    console.log("Hello, World!");
+// function greet(){
+//     console.log("Hello, World!");
+// }
+// greet();
+
+
+// IIFI method
+// (function greet(){
+//     console.log("Hello, World!");
+// })();
+
+// function add(a,b){
+//     console.log(a+b);
+// }
+// add(5,7);
+// function add(a,b){
+//     return a+b;
+
+// }
+// const result=add(5,7);
+// console.log(result);
+
+function add(){
+    // console.log(arguments)
+    console.log(arguments[0])
 }
-greet();
+add(4,5)
+
+
+
