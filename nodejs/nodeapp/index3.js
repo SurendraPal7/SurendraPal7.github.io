f1d556ca8d08f0c6db2fa02268d209432f987e46
NewUserName <ramanpal7228@gmail.com>
2025-07-03 14:16:02 +0530

    up

--- nodejs/nodeapp/index3.js
+++ nodejs/nodeapp/index3.js
@@ -23,16 +23,37 @@ app.listen(8000, () => {
 
 // app.get("/products",(req, res) => {
 //   const products=[
-//     {name:"Product 1",price:140},
-//     {name:"Product 2",price:150},
+//     {id:1,name:"Product 1",price:140},
+//     {id:2,name:"Product 2",price:150},
 
-//     {name:"Product 3",price:160},
-//     {name:"Product 5",price:140},
+//     {id:3,name:"Product 3",price:160},
+//     {id:4,name:"Product 5",price:140},
 
 
 //   ]
 //   res.json(products);
 // });
+
+
+app.get("/products/:id",(req, res) => {
+  const products=[
+    {id:1,name:"Product 1",price:140},
+    {id:2,name:"Product 2",price:150},
+
+    {id:3,name:"Product 3",price:160},
+    {id:4,name:"Product 5",price:140},
+
+
+  ]
+  const pid=req.params.id;
+  const product=products.find((p)=>p.id==pid);
+  if(product){
+    res.json(product);
+    }
+    else{
+      res.status(404).json({error:"Product not found"});
+    }
+});
 
 
 
@@ -56,6 +77,20 @@ app.listen(8000, () => {
 //   res.send(req.params.name+req.params.age);
 // })
 
-app.get("/:name/:age",(req,res)=>{
-  res.send(req.params.name+req.params.age);
-})
+// app.get("/:name/:age",(req,res)=>{
+//   res.send(req.params.name+req.params.age);
+// })
+
+// app.get("/",(req,res)=>{
+//   res.send("Hello from server");
+// })
+
+
+// app.get("/",(req,res)=>{
+//   res.send(req.headers.authorization);
+// })
+
+
+// app.get('/',(req,res)=>{
+//   res.send(req.query.name+req.query.age)
+// })
