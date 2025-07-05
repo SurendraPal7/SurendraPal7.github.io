36939c97ffc2105d8a13262160b1859c1ac9da39
NewUserName <ramanpal7228@gmail.com>
2025-07-05 15:15:06 +0530

    updated

--- nodejs/nodeapp/index12.js
+++ nodejs/nodeapp/index12.js
@@ -4,21 +4,25 @@ const userRouter = express.Router();
 const productRouter = express.Router();
 
 
+
 app.listen(8000, () => {
     console.log("Server started on port 8000");
 });
-app.get('/api/users/show', (req, res) => {
+userRouter.get('/api/users/show', (req, res) => {
     res.send('users show endpoint!');
 })
-app.post('/api/users/register',(req,res)=>{
+userRouter.post('/api/users/register',(req,res)=>{
     res.send('User registered successfully!');
 })
-app.post('/api/users/login',(req,res)=>{
+userRouter.post('/api/users/login',(req,res)=>{
     res.send('User logged in successfully!');
 });
-app.get("/api/products/show",(req,res)=>{
+productRouter.get("/api/products/show",(req,res)=>{
     res.send('products show endpoint!');
 })
-app.delete("/api/products/:id",(req,res)=>{
+productRouter.delete("/api/products/:id",(req,res)=>{
     res.send('Product deleted');
 })
+
+app.use('/api/users',userRouter);
+app.use('/api/products',productRouter);
